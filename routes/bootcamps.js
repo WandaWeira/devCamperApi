f8ca086dc9043e95dec0baf273c16baf09cc8584
wanda <rashiidahweira@gmail.com>
2023-11-18 15:13:54 +0300

    Express router

---
+++ routes/bootcamps.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Create routes
+router.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Show all bootcamps" });
+});
+
+router.get("/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Get a bootcamp of id ${req.params.id}` });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Create new bootcamp" });
+});
+
+router.put("/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete bootcamp of id ${req.params.id}` });
+});
+
+module.exports = router;
